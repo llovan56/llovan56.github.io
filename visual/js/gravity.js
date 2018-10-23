cf19da644252faff7aeb4c0f0495512261100248
llovan56 <llovan56@gmail.com>
2018-10-22 21:30:04 -0500

    UI/UX updates

--- visual/js/gravity.js
+++ visual/js/gravity.js
@@ -12,7 +12,7 @@ function initialize() {
         device = query.device || '';
 
     // kick everything off with the right device
-    // init(device);
+    init(device);
 
     function getQueryParams(qs) {
         qs = qs.split('+').join(' ');
@@ -62,7 +62,21 @@ function initialize() {
 
 // Animations
 function frameOne() {
-    this.$('#circle1').fadeIn(500, function () {
+    this.$('#nada').fadeIn(100, function () {
+        $('#nada').animate({
+            'height': '300px',
+            'width': '300px'
+        });
+
+        $('#nada').delay(1000).fadeOut(100, function () {
+            $('#nada').animate({
+                'height': '320px',
+                'width': '320px'
+            });
+        });
+    });
+
+    this.$('#circle1').fadeIn(200, function () {
         $('#circle1').animate({
             'height': '1500px',
             'opacity': '0',
@@ -70,7 +84,7 @@ function frameOne() {
         });
     });
 
-    this.$('#circle2').delay(500).fadeIn(500, function () {
+    this.$('#circle2').delay(200).fadeIn(200, function () {
         $('#circle2').animate({
             'height': '1500px',
             'opacity': '0',
@@ -78,7 +92,7 @@ function frameOne() {
         });
     });
 
-    this.$('#circle3').delay(1000).fadeIn(500, function () {
+    this.$('#circle3').delay(400).fadeIn(200, function () {
         $('#circle3').animate({
             'height': '1500px',
             'opacity': '0',
@@ -86,68 +100,23 @@ function frameOne() {
         });
     });
 
-    this.$('#circle4').delay(1500).fadeIn(500, function () {
+    this.$('#circle4').delay(600).fadeIn(200, function () {
         $('#circle4').animate({
             'height': '1500px',
             'opacity': '0',
             'width': '1500px'
         });
     });
-
-    this.$('#nada').delay(2000).fadeIn(200, function () {
-        $('#nada').animate({
-            'height': '300px',
-            'width': '300px'
-        });
-        $('#nada').delay(2000).fadeOut(200, function() {
-            $('#nada').animate({
-                'height': '320px',
-                'width': '320px'
-            });
-        });
-    });
     
 };
 
 function frameTwo() {
-    this.$('#circle5').delay(2200).fadeIn(500, function () {
-        $('#circle5').animate({
-            'height': '1500px',
-            'opacity': '0',
-            'width': '1500px'
-        });
-    });
-
-    this.$('#circle6').delay(2700).fadeIn(500, function () {
-        $('#circle6').animate({
-            'height': '1500px',
-            'opacity': '0',
-            'width': '1500px'
-        });
-    });
-
-    this.$('#circle7').delay(3200).fadeIn(500, function () {
-        $('#circle7').animate({
-            'height': '1500px',
-            'opacity': '0',
-            'width': '1500px'
-        });
-    });
-
-    this.$('#circle8').delay(3700).fadeIn(500, function () {
-        $('#circle8').animate({
-            'height': '1500px',
-            'opacity': '0',
-            'width': '1500px'
-        });
-    });
-
-    this.$('#zip').delay(4200).fadeIn(200, function () {
+    this.$('#zip').delay(1000).fadeIn(100, function () {
         $('#zip').animate({
             'height': '300px',
             'width': '300px'
         });
-        $('#zip').delay(2000).fadeOut(200, function () {
+        $('#zip').delay(1000).fadeOut(100, function () {
             $('#zip').animate({
                 'height': '320px',
                 'opacity': '0',
@@ -155,10 +124,55 @@ function frameTwo() {
             });
         });
     });
+
+    this.$('#circle5').delay(800).fadeIn(200, function () {
+        $('#circle5').animate({
+            'height': '1500px',
+            'opacity': '0',
+            'width': '1500px'
+        });
+    });
+
+    this.$('#circle6').delay(1000).fadeIn(200, function () {
+        $('#circle6').animate({
+            'height': '1500px',
+            'opacity': '0',
+            'width': '1500px'
+        });
+    });
+
+    this.$('#circle7').delay(1200).fadeIn(200, function () {
+        $('#circle7').animate({
+            'height': '1500px',
+            'opacity': '0',
+            'width': '1500px'
+        });
+    });
+
+    this.$('#circle8').delay(1400).fadeIn(200, function () {
+        $('#circle8').animate({
+            'height': '1500px',
+            'opacity': '0',
+            'width': '1500px'
+        });
+    });
 };
 
 function frameThree() {
-    this.$('#circle9').delay(4400).fadeIn(500, function () {
+    this.$('#zilch').delay(2000).fadeIn(200, function () {
+        $('#zilch').animate({
+            'height': '300px',
+            'width': '300px'
+        });
+        $('#zilch').delay(1000).fadeOut(200, function () {
+            $('#zilch').animate({
+                'height': '320px',
+                'width': '320px'
+            });
+        });
+    });
+
+    this.$('#circle9').delay(1400).fadeIn(200, function () {
         $('#circle9').animate({
             'height': '1500px',
             'opacity': '0',
@@ -166,7 +180,7 @@ function frameThree() {
         });
     });
 
-    this.$('#circle10').delay(4900).fadeIn(500, function () {
+    this.$('#circle10').delay(1600).fadeIn(200, function () {
         $('#circle10').animate({
             'height': '1500px',
             'opacity': '0',
@@ -174,7 +188,7 @@ function frameThree() {
         });
     });
 
-    this.$('#circle11').delay(5400).fadeIn(500, function () {
+    this.$('#circle11').delay(1800).fadeIn(200, function () {
         $('#circle11').animate({
             'height': '1500px',
             'opacity': '0',
@@ -182,30 +196,29 @@ function frameThree() {
         });
     });
 
-    this.$('#circle12').delay(5900).fadeIn(500, function () {
+    this.$('#circle12').delay(2000).fadeIn(200, function () {
         $('#circle12').animate({
             'height': '1500px',
             'opacity': '0',
             'width': '1500px'
         });
     });
-
-    this.$('#zilch').delay(6400).fadeIn(200, function () {
-        $('#zilch').animate({
-            'height': '300px',
-            'width': '300px'
-        });
-        $('#zilch').delay(3500).fadeOut(200, function () {
-            $('#zilch').animate({
-                'height': '320px',
-                'width': '320px'
-            });
-        });
-    });
 };
 
 function frameFour() {
-    this.$('#circle13').delay(6600).fadeIn(500, function () {
+    this.$('#zero').delay(3000).fadeIn(200, function () {
+        $('#zero').animate({
+            'height': '300px',
+            'width': '300px'
+        });
+        $('#zero').delay(1000).fadeOut(200, function () {
+            $('#zero').animate({
+                'opacity': '0',
+            });
+        });
+    });
+
+    this.$('#circle13').delay(2400).fadeIn(200, function () {
         $('#circle13').animate({
             'height': '1400px',
             'opacity': '0.8',
@@ -213,7 +226,7 @@ function frameFour() {
         });
     });
 
-    this.$('#circle14').delay(7100).fadeIn(500, function () {
+    this.$('#circle14').delay(2600).fadeIn(200, function () {
         $('#circle14').animate({
             'height': '1200px',
             'opacity': '0.6',
@@ -221,48 +234,45 @@ function frameFour() {
         });
     });
 
-    this.$('#circle15').delay(7600).fadeIn(500, function () {
+    this.$('#circle15').delay(2800).fadeIn(200, function () {
         $('#circle15').animate({
             'height': '1000px',
-            'opacity': '0.4',
+            'opacity': '0.6',
             'width': '1000px'
         });
     });
 
-    this.$('#circle16').delay(8100).fadeIn(500, function () {
+    this.$('#circle16').delay(3000).fadeIn(200, function () {
         $('#circle16').animate({
-            'height': '800px',
-            'opacity': '0.2',
-            'width': '800px'
+            'height': '750px',
+            'opacity': '0.8',
+            'width': '750px'
         });
     });
 
-    this.$('#zero').delay(8600).fadeIn(200, function () {
-        $('#zero').animate({
-            'height': '300px',
-            'width': '300px'
-        });
-        $('#zero').delay(1500).fadeOut(200, function () {
-            $('#zero').animate({
-                'opacity': '0',
-            });
+    this.$('#circle17').delay(3000).fadeIn(200, function () {
+        $('#circle17').animate({
+            'height': '600px',
+            'opacity': '1',
+            'width': '600px'
         });
     });
 
-    this.$('#zero-headline').delay(8900).fadeIn(200, function () {
+    this.$('#zero-headline').delay(5000).fadeIn(200, function () {
         $('#zero-headline').animate({
             'height': '300px',
+            'opacity': '1',
             'width': '300px'
         });
         // changes in Frame 5
-        $('#zero-headline').delay(1500).animate({
-            'bottom': '16%',
+        $('#zero-headline').delay(300).animate({
             'height': '500px',
+            'right': '18%',
             'width': '500px'
         });
     });
 
-    this.$('#bg-switch').delay(9900).fadeIn(700, function () {
+    this.$('#bg-switch').delay(6300).fadeIn(500, function () {
         $('#bg-switch').animate({
             'opacity': '1'
         });
@@ -270,27 +280,33 @@ function frameFour() {
 };
 
 function frameFive() {
-    this.$('.text1').delay(11400).fadeIn(700, function () {
+    this.$('.text1').delay(6300).fadeIn(500, function () {
         $('.text1').animate({
-            'left': '30%',
+            'left': '27%',
             'opacity': '1'
         });
     });
 
-    this.$('.text2').delay(11400).fadeIn(700, function () {
+    this.$('.text2').delay(6300).fadeIn(500, function () {
         $('.text2').animate({
             'opacity': '1',
-            'right': '24%'
+            'right': '33%'
         });
     });
 
-    this.$('.cta').delay(11400).fadeIn(700, function () {
+    this.$('.logo').delay(6300).fadeIn(500, function () {
+        $('.logo').animate({
+            'opacity': '1'
+        });
+    });
+
+    this.$('.cta').delay(6300).fadeIn(500, function () {
         $('.cta').animate({
             'opacity': '1'
         });
     });
 
-    this.$('.legal').delay(11400).fadeIn(700, function () {
+    this.$('.legal').delay(6100).fadeIn(500, function () {
         $('.legal').animate({
             'opacity': '1'
         });
