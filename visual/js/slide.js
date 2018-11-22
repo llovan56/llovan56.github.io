387c5f3c75b677fcb922ad82223839383c23a4ff
llovan56 <llovan56@gmail.com>
2018-11-21 23:09:41 -0600

    adding animation

--- visual/js/slide.js
+++ visual/js/slide.js
@@ -8,23 +8,26 @@ function showImages(el) {
         var topOfWindow = $(window).scrollTop();
         if (topOfWindow + windowHeight - 200 > thisPos ) {
             $(this).addClass("fadeIn");
-            
-                // $('.headline').fadeIn(2000, function () {
-                //     $('.headline').animate({
-                //         'opacity': '1'
-                //     });
-                // });
+            leftSlide();
             
         }
     });
-  }
+}
+
+function leftSlide() {
+    $('.headline').fadeIn(2000, function () {
+        $('.headline').animate({
+            'left': '300px'
+        });
+    });
+}
   
-  // if the image in the window of browser when the page is loaded, show that image
-  $(document).ready(function(){
-        showImages('.onScreen,.onScreen-bottom');
-  });
+// if the image in the window of browser when the page is loaded, show that image
+$(document).ready(function(){
+    showImages('.onScreen,.onScreen-bottom');
+});
   
-  // if the image in the window of browser when scrolling the page, show that image
-  $(window).scroll(function() {
-        showImages('.onScreen,.onScreen-bottom');
-  });
+// if the image in the window of browser when scrolling the page, show that image
+$(window).scroll(function() {
+    showImages('.onScreen,.onScreen-bottom');
+});
