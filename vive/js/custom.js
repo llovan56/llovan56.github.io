cef08818c71be5a21686b6001ab880385327dc4b
llovan56 <llovan56@gmail.com>
2019-05-06 22:44:09 -0500

    vive updates

--- vive/js/custom.js
+++ vive/js/custom.js
@@ -1,25 +1,80 @@
-
-
-
 // shop page show/hide sections
 
 var greenTea = document.querySelector('#green-tea'),
     blackTea = document.querySelector('#black-tea'),
-    shopIntro = document.querySelector('.shop-intro'),
+    whiteTea = document.querySelector('#white-tea'),
+    oolongTea = document.querySelector('#oolong-tea'),
+    herbalTea = document.querySelector('#herbal-tea'),
+    yellowTea = document.querySelector('#yellow-tea'),
+    // shopIntro = document.querySelectorAll('.shop-intro'),
+    sectionOne = document.querySelector('#section-one'),
     sectionTwo = document.querySelector('#section-two'),
     sectionThree = document.querySelector('#section-three'),
+    sectionFour = document.querySelector('#section-four'),
+    sectionFive = document.querySelector('#section-five'),
+    sectionSix = document.querySelector('#section-six'),
+    hide = document.querySelector('.hide'),
     show = document.querySelector('.show');
 
+
 greenTea.addEventListener("click", function () {
-    shopIntro.classList.add("hidden");
-    sectionTwo.classList.add("show");
+    sectionOne.classList.add('show');
+    sectionTwo.classList.remove("show");
+    sectionThree.classList.remove("show");
+    sectionFour.classList.remove("show");
+    sectionFive.classList.remove("show");
+    sectionSix.classList.remove("show");
 });
 
 blackTea.addEventListener("click", function () {
-    shopIntro.classList.add("hidden");
-    sectionTwo.classList.add("hidden");
-    sectionThree.classList.add("show");
+    sectionOne.classList.add('hide');
+    sectionOne.classList.remove("show");
+    sectionTwo.classList.add("show");
+    sectionThree.classList.remove("show");
+    sectionFour.classList.remove("show");
+    sectionFive.classList.remove("show");
+    sectionSix.classList.remove("show");
 });
 
+whiteTea.addEventListener("click", function () {
+    sectionOne.classList.add('hide');
+    sectionOne.classList.remove("show");
+    sectionTwo.classList.remove("show");
+    sectionThree.classList.add("show");
+    sectionFour.classList.remove("show");
+    sectionFive.classList.remove("show");
+    sectionSix.classList.remove("show");
+});
+
+oolongTea.addEventListener("click", function () {
+    sectionOne.classList.add('hide');
+    sectionOne.classList.remove("show");
+    sectionTwo.classList.remove("show");
+    sectionThree.classList.remove("show");
+    sectionFour.classList.add("show");
+    sectionFive.classList.remove("show");
+    sectionSix.classList.remove("show");
+});
+
+herbalTea.addEventListener("click", function () {
+    sectionOne.classList.add('hide');
+    sectionOne.classList.add('hide');
+    sectionOne.classList.remove("show");
+    sectionTwo.classList.remove("show");
+    sectionThree.classList.remove("show");
+    sectionFour.classList.remove("show");
+    sectionFive.classList.add("show");
+    sectionSix.classList.remove("show");
+});
+
+yellowTea.addEventListener("click", function () {
+    sectionOne.classList.add('hide');
+    sectionOne.classList.remove("show");
+    sectionTwo.classList.remove("show");
+    sectionThree.classList.remove("show");
+    sectionFour.classList.remove("show");
+    sectionFive.classList.remove("show");
+    sectionSix.classList.add("show");
+});
 
 
